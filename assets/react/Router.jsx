b0b47e7fc7eb445015b9e385227e009309b97b5b
Alphayra <zefinthefly@gmail.com>
2026-03-08 00:36:55 +0100

    ✨ Ajout squelettes des pages principales et routes

--- assets/react/Router.jsx
+++ assets/react/Router.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/nav/navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Challenge from "./components/Challenge/Challenge";
 import Classement from "./components/Classement/Classement";
 import Profil from "./components/Profil/Profil";
+import Contact from "./components/Contact/Contact";
 
 export default function Router() {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/challenge" element={<Challenge />} />
                 <Route path="/classement" element={<Classement />} />
                 <Route path="/profil" element={<Profil />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ assets/react/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>Bienvenue</h1>
+            <p>Page d'accueil</p>
+        </div>
+    );
+}

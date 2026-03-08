ea0b5511895365b0a4b34b9d0ebe90a546e42db3
Safiya Jaouahir <safiya@air-de-safiya.home>
2026-03-08 13:38:46 +0100

    ajout des fonction de challengeController

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PreuveRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,61 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(PreuveRepository $preuveRepository, UserRepository $userRepository): Response
     {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+        if(!$user) return $this->redirectToRoute('app_login');
+
+        // afficher les defis
+        $preuves = $preuveRepository->findBy(['user' => $user]);
+        $defiDt = [];
+        foreach($preuves as $preuve){
+            $defiDt[] = [
+                'id' => $preuve->getDefi()->getId(),
+                'titre' => $preuve->getDefi()->getTitre(),
+                'statut' => strtolower($preuve->getStatus()), 
+                'date' => $preuve->getDateEnvoi()->format('d/m/Y')
+            ];
+        }
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
+
+        // afficher les informations de l'équipe
+        $equipe = $user->getEquipe();
+        $equipeDt = $equipe ? [
+            'nom' => $equipe->getNom(),
+            'score' => $equipe->getScoreEquipe(),
+            'membres' => count($equipe->getUsers())
+        ] : null ;
+
+        // afficher les derniers scores gagnés par le user
+        $dernieresPreuves = $preuveRepo->findBy(
+            ['utilisateur' => $user, 'statut' => 'VALIDE'],
+            ['dateEnvoi' => 'DESC'], 
+            5
+        );
+
+        $scoresDt = [];
+        foreach ($dernieresPreuves as $preuve) {
+            $scoresDt[] = [
+                'defiTitre' => $preuve->getDefi()->getTitre(),
+                'points' => $preuve->getDefi()->getPoint(),
+                'date' => $preuve->getDateEnvoi()->format('d/m')
+            ];
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+            'userDt' => [
+                'nom' => $user->getNom(),
+                'scoreTotal' => $user->getScoreTotal(),
+                'totalCO2' => $user->getTotalCO2(),
+            ],
+            'defis' => $defisDt,
+            'equipe' => $equipeDt,
+            'scores' => $scoresDt
+        ]);
     }
+
 }

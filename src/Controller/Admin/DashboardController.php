760f5d2cfe526e7d930a0b056656f2278f34f2fa
Safiya Jaouahir <safiya@air-de-safiya.home>
2026-03-08 14:54:59 +0100

    ajout fonctionnalité leaderboardcontroller et crud pour user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Preuve;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -29,6 +30,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Preuve', 'fas fa-list', Preuve::class);
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-list', User::class);
     }
 
     public function configureAssets(): Assets
--- src/Controller/LeaderboardController.php
+++ src/Controller/LeaderboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\EquipeRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,50 @@ use Symfony\Component\Routing\Attribute\Route;
 final class LeaderboardController extends AbstractController
 {
     #[Route('/leaderboard', name: 'app_leaderboard')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, EquipeRepository $equipeRepository): Response
     {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+        if (!$user) return $this->redirectToRoute('app_login');
+
+        // pour le classement des 10 meilleures utilisateur
+        $topUsers = $userRepository->findBy([], ['scoreTotal' => 'DESC'], 10);
+        $usersDt = [];
+        foreach ($topUsers as $index => $u) {
+            $usersDt[] = [
+                'rang' => $index + 1,
+                'nom' => $u->getNom(),
+                'points' => $u->getScoreTotal(),
+                'isMe' => ($u->getId() === $user->getId())
+            ];
+        }
+
+        // pour le classement des 10 meilleures équipes
+        $topEquipes = $equipeRepository->findBy([], ['scoreEquipe' => 'DESC'], 10);
+        $equipesDt = [];
+        foreach ($topEquipes as $index => $e) {
+            $equipesDt[] = [
+                'rang' => $index + 1,
+                'nom' => $e->getNom(),
+                'scores' => $e->getScoreEquipe(),
+                'isMyTeam' => ($user->getEquipe() && $e->getId() === $user->getEquipe()->getId())
+            ];
+        }
+
         return $this->render('leaderboard/index.html.twig', [
-            'controller_name' => 'LeaderboardController',
+            'usersRang' => $usersDt,
+            'equipesRang' => $equipesDt,
+            'monRang' => $this->findUserRank($user, $userRepository),
         ]);
+    }
+
+    // fonction pour avoir le rang du user connecté
+    private function findUserRank($user, $userRepository): int
+    {
+        $allUsers = $userRepository->findBy([], ['scoreTotal' => 'DESC']);
+        foreach ($allUsers as $index => $u) {
+            if ($u->getId() === $user->getId()) return $index + 1;
+        }
+        return 0;
     }
 }

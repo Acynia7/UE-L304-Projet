c824b815b02ff21bc1a7e719beaf1a6e3849fd64
Alphayra <zefinthefly@gmail.com>
2026-03-11 13:03:07 +0100

    ✨ Ajout page Home

---
+++ assets/react/components/Home/HomeHero.jsx
@@ -0,0 +1,16 @@
+import './home.scss';
+
+export default function HomeHero() {
+    return (
+        <div className="home__hero">
+            <div className="o-container o-container--centered">
+                <div className="c-title c-title--main">
+                    <h1>EcoBattle</h1>
+                </div>
+                <div className="c-title c-title--sub-main">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+            </div>
+        </div>
+    );
+}

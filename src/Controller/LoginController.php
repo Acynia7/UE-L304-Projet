08498ee3ec76c0dca448e0acd4e9680e7976b212
Safiya Jaouahir <safiya@air-de-safiya.home>
2026-03-16 16:18:39 +0100

    api login et profile

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,31 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function login(#[CurrentUser] ?User $user): JsonResponse
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('login/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
+        if (null === $user) {
+            return new JsonResponse([
+                'error' => 'Identifiants invalides ou utilisateur non trouvé.'
+            ], 401);
+        }
+        return new JsonResponse([
+            'user'  => $user->getUserIdentifier(),
+            'nom' => $user->getNom(),
+            'roles' => $user->getRoles(),
         ]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route('/api/logout', name: 'api_logout', methods: ['POST'])]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \Exception('Ceci ne devrait pas être atteint');
     }
 }

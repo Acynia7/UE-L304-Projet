348ad8363245f6e5bf9a07f001928b15302c0635
Alphayra <zefinthefly@gmail.com>
2026-03-17 18:54:47 +0100

    ♻️ Déplacement de composants
    Déplacement de composants venant de home vers des composants généraux à but de les réutiliser, modification des styles nécessaires et documents nécessaire à leur implémentation.

--- assets/react/components/Home/Home.jsx
+++ assets/react/components/Home/Home.jsx
@@ -1,29 +1,30 @@
 import HomeHero from "./HomeHero";
-import HomeSideContentL from "./HomeSideContentL";
+import SideContentL from "../contentDisplay/SideContentL";
+import SideContentR from "../contentDisplay/SideContentR";
+import Spacer from "../contentDisplay/Spacer";
+
 import './Home.scss';
-import HomeSideContentR from "./HomeSideContentR";
-import HomeSpacer from "./HomeSpacer";
 
 export default function Home() {
     return (
         <div className="home">
             <HomeHero />
-            <HomeSideContentL alt='undefined'> 
+            <SideContentL alt='undefined'> 
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vel labore ex minima, 
                 veritatis iste quasi! Quod, placeat sequi commodi mollitia, sint ducimus sunt corrupti 
                 debitis quisquam necessitatibus unde cupiditate dolor repellendus neque nihil sit 
                 laboriosam quos laborum aliquid ad! Necessitatibus provident voluptatum voluptates 
                 accusamus, qui dicta nisi aut velit! 
-            </HomeSideContentL>
-            <HomeSpacer size='60' />
-            <HomeSideContentR alt='undefined'>
+            </SideContentL>
+            <Spacer size='60' />
+            <SideContentR alt='undefined'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sequi voluptates 
                 doloremque cumque repellat numquam. Obcaecati veritatis est dolorum repudiandae vero 
                 iste explicabo ut quod soluta harum dignissimos, accusamus inventore dolore, aliquid 
                 eos dicta minus. Quibusdam iusto sequi accusamus architecto autem sint necessitatibus? 
                 Esse, assumenda vel culpa possimus veritatis pariatur.    
-            </HomeSideContentR>
-
+            </SideContentR>
+            <Spacer size='60' />
         </div>
     );
 }
